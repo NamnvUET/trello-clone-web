f0ccd0dab348941677f592701251d28810c2f5d3
namnv <nguyenvannamk59@gmail.com>
2022-07-13 15:02:42 +0700

    done tach component

--- src/App.js
+++ src/App.js
@@ -1,119 +1,15 @@
 import React from 'react';
 import './App.scss';
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="trello-clone-master">
-      <nav className='navbar app'>
-        App Bar
-      </nav>
-      <nav className='navbar board'>
-        Board Bar
-      </nav>
-      <div className='board-columns'>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="img/img_demo.jpg" alt="trello-clone-img" />
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div><div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div><div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div><div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
-              Title: CCS_CN96
-            </li>
-            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent/>
     </div>
   );
 }
