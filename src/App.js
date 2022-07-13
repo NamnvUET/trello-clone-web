25fed0f8c5769a6618b06506c8597bbdfef2fb27
namnv <nguyenvannamk59@gmail.com>
2022-07-13 10:59:42 +0700

    khung giao dien co ban

--- src/App.js
+++ src/App.js
@@ -1,7 +1,120 @@
+import React from 'react';
+import './App.scss';
 
 function App() {
   return (
-    <h1>hrhrhhrh</h1>
+    <div className="trello-clone-master">
+      <nav className='navbar app'>
+        App Bar
+      </nav>
+      <nav className='navbar board'>
+        Board Bar
+      </nav>
+      <div className='board-columns'>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="img/img_demo.jpg" alt="trello-clone-img" />
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div><div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div><div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div><div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src="img/img_demo.jpg" alt="trello-clone-img" /> */}
+              Title: CCS_CN96
+            </li>
+            <li>jrjrtjrtjtrjrjrjtrjtrjr</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+            <li>rtjjrjjrjrjtrjrtjtrjtrjrtjrtj</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
   );
 }
 
